409e28bdd6b78c5ea51ccae87aa37fdd33a3e9ed
jeemyeong <jeemyeong@gmail.com>
2017-09-25 01:10:44 +0900

    1373

--- boj/1373.py
+++ boj/1373.py
@@ -1,6 +1,6 @@
 def solve(str_binary):
     bin_to_oct = {"000":"0", "001":"1", "010":"2", "011":"3", "100":"4", "101":"5", "110":"6", "111":"7"}
-    
+
     if len(str_binary)%3 == 1:
         str_binary = "00"+str_binary
     elif len(str_binary)%3 == 2:
