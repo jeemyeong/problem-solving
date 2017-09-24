9446e3db2854215893d299c826c08fcd3964cf64
Jeemyeong Lee <jeemyeonglee@Jeemyeongs-MacBook-Pro.local>
2017-09-24 23:42:56 +0900

    2240

--- boj/boj-2240.py
+++ boj/boj-2240.py
@@ -1,28 +1,20 @@
-# Not solved
+def get_item_without_null_exception(i, k, p, dp):
+    if i < 0 or k < 0 or p < 0:
+        return 0
+    else:
+        return dp[i][k][p]
+
 def solve(T, W, order):
-    import heapq
-    q = []
-    heapq.heappush(q, (W, 1 if 1 == order[0] else 0, 0, 1))
-    heapq.heappush(q, (W, 1 if 2 == order[0] else 0, 0, 2))
     dp = [[[0]*2 for _ in range(W+1)] for _ in range(T)]
-    while q:
-        left_w, count, index, position = heapq.heappop(q)
-        if count < dp[index][left_w][position-1]:
-            continue
-        dp[index][left_w][position-1] = count
-        if index == T-1:
-            continue
-        if position == 1:
-            heapq.heappush(q, (left_w, count + (1 if order[index+1] == 1 else 0), index+1, 1))
-            if left_w > 0:
-                heapq.heappush(q, (left_w-1, count + (1 if order[index+1] == 2 else 0), index+1, 2))
-        if position == 2:
-            heapq.heappush(q, (left_w, count + (1 if order[index+1] == 2 else 0), index+1, 2))
-            if left_w > 0:
-                heapq.heappush(q, (left_w-1, count + (1 if order[index+1] == 1 else 0), index+1, 1))
-    # for i in dp:
-    #     print(i)
-    # print()
+
+    for i in range(0, T):
+        for k in range(W+1):
+            if order[i] == 1:
+                dp[i][k][0] = max(get_item_without_null_exception(i-1, k, 0, dp), get_item_without_null_exception(i-1, k-1, 1, dp)) + 1
+                dp[i][k][1] = max(get_item_without_null_exception(i-1, k, 1, dp), get_item_without_null_exception(i-1, k-1, 0, dp))
+            else:
+                dp[i][k][0] = max(get_item_without_null_exception(i-1, k, 0, dp), get_item_without_null_exception(i-1, k-1, 1, dp))
+                dp[i][k][1] = max(get_item_without_null_exception(i-1, k, 1, dp), get_item_without_null_exception(i-1, k-1, 0, dp)) + 1
     return max(max(dp[T-1], key=lambda x: max(x)))
 
 def run():
