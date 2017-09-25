119e1a1ad55d6524fb348daeea2f17ace7caf033
jeemyeong <jeemyeong@gmail.com>
2017-09-26 01:15:55 +0900

    Refactor

--- boj/test-maker.py
+++
@@ -1,8 +0,0 @@
-import random
-
-n = random.randint(2,100)
-print(n)
-with open('2261-4.input', 'w') as f:
-    f.write(str(n) + "\n")
-    for _ in range(n):
-        f.write(str(random.randint(-1000,1000)) + " " + str(random.randint(-1000,1000)) + "\n")
