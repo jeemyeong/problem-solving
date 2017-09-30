426c828d2d542e0be3cd1d120904460d480055f5
jeemyeong <jeemyeong@gmail.com>
2017-09-30 17:05:03 +0900

    13459

--- boj/13459/13460.py
+++ boj/13459/13460.py
@@ -1,115 +1,49 @@
-def solve(N, M, _map):
+def move(grid, cur, d, R, B):
+    while True:
+        if grid[cur[0]+d[0]][cur[1]+d[1]] == '#' or (cur[0]+d[0], cur[1]+d[1]) in (R, B):
+            return cur
+        if grid[cur[0]+d[0]][cur[1]+d[1]] == 'O':
+            return (-1, -1)
+        cur = (cur[0]+d[0], cur[1]+d[1])
+
+def solve(N, M, grid):
     from collections import deque
+    R = None
+    B = None
+    for i in range(N):
+        for j in range(M):
+            if grid[i][j] == 'R':
+                R = (i, j)
+            elif grid[i][j] == 'B':
+                B = (i, j)
+
     q = deque([])
-    q.append((_map, 0))
-    visited = dict()
-    while q:
-        _map, moves = q.popleft()
-        if str(_map) in visited or moves >= 10:
-            continue
-        visited[str(_map)] = 1
-        new_map = [[e for e in row] for row in _map]
-        red_into_the_hole = False
-        blue_into_the_hole = False
-        for i in range(0, N-1):
-            for j in range(0, M-1):
-                for k in range(i, 0, -1):
-                    if new_map[k][j] != 'R' and new_map[k][j] != 'B':
-                        break
-                    if new_map[k-1][j] == '.':
-                        temp = new_map[k][j]
-                        new_map[k][j] = new_map[k-1][j]
-                        new_map[k-1][j] = temp
-                    elif new_map[k][j] == 'R' and new_map[k-1][j] == 'O':
-                        new_map[k][j] = '.'
-                        red_into_the_hole = True
-                    elif new_map[k][j] == 'B' and new_map[k-1][j] == 'O':
-                        new_map[k][j] = '.'
-                        blue_into_the_hole = True
-        if red_into_the_hole is True and blue_into_the_hole is False:
-            return moves+1
-        if red_into_the_hole is False and blue_into_the_hole is False:
-            q.append((new_map, moves+1))
-        
-        new_map = [[e for e in row] for row in _map]
-        red_into_the_hole = False
-        blue_into_the_hole = False
-        for i in range(N-1, 0, -1):
-            for j in range(M-1, 0, -1):
-                for k in range(i, N, 1):
-                    if new_map[k][j] != 'R' and new_map[k][j] != 'B':
-                        break
-                    if new_map[k+1][j] == '.':
-                        temp = new_map[k][j]
-                        new_map[k][j] = new_map[k+1][j]
-                        new_map[k+1][j] = temp
-                    elif new_map[k][j] == 'R' and new_map[k+1][j] == 'O':
-                        new_map[k][j] = '.'
-                        red_into_the_hole = True
-                    elif new_map[k][j] == 'B' and new_map[k+1][j] == 'O':
-                        new_map[k][j] = '.'
-                        blue_into_the_hole = True
-        if red_into_the_hole is True and blue_into_the_hole is False:
-            return moves+1
-        if red_into_the_hole is False and blue_into_the_hole is False:
-            q.append((new_map, moves+1))
-
-        new_map = [[e for e in row] for row in _map]
-        red_into_the_hole = False
-        blue_into_the_hole = False
-        for i in range(0, N-1):
-            for j in range(0, M-1):
-                for k in range(j, 0, -1):
-                    if new_map[i][k] != 'R' and new_map[i][k] != 'B':
-                        break
-                    if new_map[i][k-1] == '.':
-                        temp = new_map[i][k]
-                        new_map[i][k] = new_map[i][k-1]
-                        new_map[i][k-1] = temp
-                    elif new_map[i][k] == 'R' and new_map[i][k-1] == 'O':
-                        new_map[i][k] = '.'
-                        red_into_the_hole = True
-                    elif new_map[i][k] == 'B' and new_map[i][k-1] == 'O':
-                        new_map[i][k] = '.'
-                        blue_into_the_hole = True
-        if red_into_the_hole is True and blue_into_the_hole is False:
-            return moves+1
-        if red_into_the_hole is False and blue_into_the_hole is False:
-            q.append((new_map, moves+1))
-        
-        new_map = [[e for e in row] for row in _map]
-        red_into_the_hole = False
-        blue_into_the_hole = False
-        for i in range(N-1, 0, -1):
-            for j in range(M-1, 0, -1):
-                for k in range(j, M, 1):
-                    if new_map[i][k] != 'R' and new_map[i][k] != 'B':
-                        break
-                    if new_map[i][k+1] == '.':
-                        temp = new_map[i][k]
-                        new_map[i][k] = new_map[i][k+1]
-                        new_map[i][k+1] = temp
-                    elif new_map[i][k] == 'R' and new_map[i][k+1] == 'O':
-                        new_map[i][k] = '.'
-                        red_into_the_hole = True
-                    elif new_map[i][k] == 'B' and new_map[i][k+1] == 'O':
-                        new_map[i][k] = '.'
-                        blue_into_the_hole = True
-        if red_into_the_hole is True and blue_into_the_hole is False:
-            return moves+1
-        if red_into_the_hole is False and blue_into_the_hole is False:
-            q.append((new_map, moves+1))
-
-
+    q.append((R, B))
+    visited = set()
+    for moves in range(10):
+        for _ in range(len(q)):
+            R, B = q.popleft()
+            if (R, B) in visited:
+                continue
+            visited.add((R, B))
+            for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                dR = move(grid, R, d, R, B)
+                dB = move(grid, B, d, dR, B)
+                dR = move(grid, dR, d, dR, dB)
+                if dB == (-1, -1):
+                    continue
+                if dR == (-1, -1):
+                    return moves+1
+                q.append((dR, dB))
     return -1
 
 def run():
     import sys
     read = sys.stdin.readline
     N, M = map(int, read().split())
-    _map = []
+    grid = []
     for _ in range(N):
-        _map.append(list(read().replace("\n", "")))
-    print(solve(N, M, _map))
+        grid.append(list(read().replace("\n", "")))
+    print(solve(N, M, grid))
 
 run()
