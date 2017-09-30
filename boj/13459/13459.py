477d2ef49752f6c1c242a77b7cdbb44d2c96faf1
jeemyeong <jeemyeong@gmail.com>
2017-09-30 15:43:41 +0900

    13459

--- boj/13459/13459.py
+++ boj/13459/13459.py
@@ -1,271 +1,106 @@
 def solve(N, M, _map):
     from collections import deque
-    R = None
-    B = None
-    for i in range(N):
-        for j in range(M):
-            if _map[i][j] == 'R':
-                R = (i, j)
-                _map[i][j] = '.'
-            elif _map[i][j] == 'B':
-                B = (i, j)
-                _map[i][j] = '.'
     q = deque([])
-    moves = 0
-    q.append((moves, R, B))
-    visited = [[[[0] * M for _ in range(N)] for _ in range(M)] for _ in range(N)]
+    q.append((_map, 0))
+    visited = dict()
     while q:
-        moves, R, B = q.popleft()
-        if visited[R[0]][R[1]][B[0]][B[1]] == 1 or moves >= 10:
+        _map, moves = q.popleft()
+        if str(_map) in visited or moves >= 10:
             continue
-        visited[R[0]][R[1]][B[0]][B[1]] = 1
-        # Moves to up
-        if R[0] >= B[0]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dB[0], 1, -1):
-                if dR[0] == (dB[0]-1) and dR[1] == dB[1]:
-                    break
-                elif _map[dB[0]-1][dB[1]] == '.':
-                    dB[0] -= 1
-                elif _map[dB[0]-1][dB[1]] == '#':
-                    break
-                elif _map[dB[0]-1][dB[1]] == 'O':
-                    blue_into_the_hole = True
-                    break
-            for i in range(dR[0], 1, -1):
-                if (dR[0]-1) == dB[0] and dR[1] == dB[1]:
-                    break
-                elif _map[dR[0]-1][dR[1]] == '.':
-                    dR[0] -= 1
-                elif _map[dR[0]-1][dR[1]] == '#':
-                    break
-                elif _map[dR[0]-1][dR[1]] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
-        elif B[0] > R[0]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dR[0], 1, -1):
-                if (dR[0]-1) == dB[0] and dR[1] == dB[1]:
-                    break
-                elif _map[dR[0]-1][dR[1]] == '.':
-                    dR[0] -= 1
-                elif _map[dR[0]-1][dR[1]] == '#':
-                    break
-                elif _map[dR[0]-1][dR[1]] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            for i in range(dB[0], 1, -1):
-                if dR[0] == (dB[0]-1) and dR[1] == dB[1]:
-                    break
-                elif _map[dB[0]-1][dB[1]] == '.':
-                    dB[0] -= 1
-                elif _map[dB[0]-1][dB[1]] == '#':
-                    break
-                elif _map[dB[0]-1][dB[1]] == 'O':
-                    blue_into_the_hole = True
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
+        visited[str(_map)] = 1
+        new_map = [[e for e in row] for row in _map]
+        red_into_the_hole = False
+        blue_into_the_hole = False
+        for i in range(0, N-1):
+            for j in range(0, M-1):
+                for k in range(i, 0, -1):
+                    if new_map[k][j] != 'R' and new_map[k][j] != 'B':
+                        break
+                    if new_map[k-1][j] == '.':
+                        temp = new_map[k][j]
+                        new_map[k][j] = new_map[k-1][j]
+                        new_map[k-1][j] = temp
+                    elif new_map[k][j] == 'R' and new_map[k-1][j] == 'O':
+                        new_map[k][j] = '.'
+                        red_into_the_hole = True
+                    elif new_map[k][j] == 'B' and new_map[k-1][j] == 'O':
+                        new_map[k][j] = '.'
+                        blue_into_the_hole = True
+        if red_into_the_hole is True and blue_into_the_hole is False:
+            return 1
+        if red_into_the_hole is False and blue_into_the_hole is False:
+            q.append((new_map, moves+1))
+        
+        new_map = [[e for e in row] for row in _map]
+        red_into_the_hole = False
+        blue_into_the_hole = False
+        for i in range(N-1, 0, -1):
+            for j in range(M-1, 0, -1):
+                for k in range(i, N, 1):
+                    if new_map[k][j] != 'R' and new_map[k][j] != 'B':
+                        break
+                    if new_map[k+1][j] == '.':
+                        temp = new_map[k][j]
+                        new_map[k][j] = new_map[k+1][j]
+                        new_map[k+1][j] = temp
+                    elif new_map[k][j] == 'R' and new_map[k+1][j] == 'O':
+                        new_map[k][j] = '.'
+                        red_into_the_hole = True
+                    elif new_map[k][j] == 'B' and new_map[k+1][j] == 'O':
+                        new_map[k][j] = '.'
+                        blue_into_the_hole = True
+        if red_into_the_hole is True and blue_into_the_hole is False:
+            return 1
+        if red_into_the_hole is False and blue_into_the_hole is False:
+            q.append((new_map, moves+1))
 
-        # Moves to down
-        if R[0] <= B[0]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dB[0], N-1, 1):
-                if dR[0] == (dB[0]+1) and dR[1] == dB[1]:
-                    break
-                elif _map[dB[0]+1][dB[1]] == '.':
-                    dB[0] += 1
-                elif _map[dB[0]+1][dB[1]] == '#':
-                    break
-                elif _map[dB[0]+1][dB[1]] == 'O':
-                    blue_into_the_hole = True
-                    break
-            for i in range(dR[0], N-1, 1):
-                if (dR[0]+1) == dB[0] and dR[1] == dB[1]:
-                    break
-                elif _map[dR[0]+1][dR[1]] == '.':
-                    dR[0] += 1
-                elif _map[dR[0]+1][dR[1]] == '#':
-                    break
-                elif _map[dR[0]+1][dR[1]] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
-        elif B[0] < R[0]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dR[0], N-1, 1):
-                if (dR[0]+1) == dB[0] and dR[1] == dB[1]:
-                    break
-                elif _map[dR[0]+1][dR[1]] == '.':
-                    dR[0] += 1
-                elif _map[dR[0]+1][dR[1]] == '#':
-                    break
-                elif _map[dR[0]+1][dR[1]] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            for i in range(dB[0], N-1, 1):
-                if dR[0] == (dB[0]+1) and dR[1] == dB[1]:
-                    break
-                elif _map[dB[0]+1][dB[1]] == '.':
-                    dB[0] += 1
-                elif _map[dB[0]+1][dB[1]] == '#':
-                    break
-                elif _map[dB[0]+1][dB[1]] == 'O':
-                    blue_into_the_hole = True
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
+        new_map = [[e for e in row] for row in _map]
+        red_into_the_hole = False
+        blue_into_the_hole = False
+        for i in range(0, N-1):
+            for j in range(0, M-1):
+                for k in range(j, 0, -1):
+                    if new_map[i][k] != 'R' and new_map[i][k] != 'B':
+                        break
+                    if new_map[i][k-1] == '.':
+                        temp = new_map[i][k]
+                        new_map[i][k] = new_map[i][k-1]
+                        new_map[i][k-1] = temp
+                    elif new_map[i][k] == 'R' and new_map[i][k-1] == 'O':
+                        new_map[i][k] = '.'
+                        red_into_the_hole = True
+                    elif new_map[i][k] == 'B' and new_map[i][k-1] == 'O':
+                        new_map[i][k] = '.'
+                        blue_into_the_hole = True
+        if red_into_the_hole is True and blue_into_the_hole is False:
+            return 1
+        if red_into_the_hole is False and blue_into_the_hole is False:
+            q.append((new_map, moves+1))
+        
+        new_map = [[e for e in row] for row in _map]
+        red_into_the_hole = False
+        blue_into_the_hole = False
+        for i in range(N-1, 0, -1):
+            for j in range(M-1, 0, -1):
+                for k in range(j, M, 1):
+                    if new_map[i][k] != 'R' and new_map[i][k] != 'B':
+                        break
+                    if new_map[i][k+1] == '.':
+                        temp = new_map[i][k]
+                        new_map[i][k] = new_map[i][k+1]
+                        new_map[i][k+1] = temp
+                    elif new_map[i][k] == 'R' and new_map[i][k+1] == 'O':
+                        new_map[i][k] = '.'
+                        red_into_the_hole = True
+                    elif new_map[i][k] == 'B' and new_map[i][k+1] == 'O':
+                        new_map[i][k] = '.'
+                        blue_into_the_hole = True
+        if red_into_the_hole is True and blue_into_the_hole is False:
+            return 1
+        if red_into_the_hole is False and blue_into_the_hole is False:
+            q.append((new_map, moves+1))
 
-        # Moves to left
-        if R[1] >= B[1]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dB[1], 1, -1):
-                if dR[0] == dB[0] and dR[1] == (dB[1]-1):
-                    break
-                elif _map[dB[0]][dB[1]-1] == '.':
-                    dB[1] -= 1
-                elif _map[dB[0]][dB[1]-1] == '#':
-                    break
-                elif _map[dB[0]][dB[1]-1] == 'O':
-                    blue_into_the_hole = True
-                    break
-            for i in range(dR[1], 1, -1):
-                if dR[0] == dB[0] and (dR[1]-1) == dB[1]:
-                    break
-                elif _map[dR[0]][dR[1]-1] == '.':
-                    dR[1] -= 1
-                elif _map[dR[0]][dR[1]-1] == '#':
-                    break
-                elif _map[dR[0]][dR[1]-1] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
-        elif B[1] > R[1]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dR[1], 1, -1):
-                if dR[0] == dB[0] and (dR[1]-1) == dB[1]:
-                    break
-                elif _map[dR[0]][dR[1]-1] == '.':
-                    dR[1] -= 1
-                elif _map[dR[0]][dR[1]-1] == '#':
-                    break
-                elif _map[dR[0]][dR[1]-1] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            for i in range(dB[1], 1, -1):
-                if dR[0] == dB[0] and dR[1] == (dB[1]-1):
-                    break
-                elif _map[dB[0]][dB[1]-1] == '.':
-                    dB[1] -= 1
-                elif _map[dB[0]][dB[1]-1] == '#':
-                    break
-                elif _map[dB[0]][dB[1]-1] == 'O':
-                    blue_into_the_hole = True
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
 
-        # Moves to right
-        if R[1] <= B[1]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dB[1], M-1, 1):
-                if dR[0] == dB[0] and dR[1] == (dB[1]+1):
-                    break
-                elif _map[dB[0]][dB[1]+1] == '.':
-                    dB[1] += 1
-                elif _map[dB[0]][dB[1]+1] == '#':
-                    break
-                elif _map[dB[0]][dB[1]+1] == 'O':
-                    blue_into_the_hole = True
-                    break
-            for i in range(dR[1], M-1, 1):
-                if dR[0] == dB[0] and (dR[1]+1) == dB[1]:
-                    break
-                elif _map[dR[0]][dR[1]+1] == '.':
-                    dR[1] += 1
-                elif _map[dR[0]][dR[1]+1] == '#':
-                    break
-                elif _map[dR[0]][dR[1]+1] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
-        elif B[1] < R[1]:
-            dR = [R[0], R[1]]
-            dB = [B[0], B[1]]
-            red_into_the_hole = False
-            blue_into_the_hole = False
-            for i in range(dR[1], M-1, 1):
-                if dR[0] == dB[0] and (dR[1]+1) == dB[1]:
-                    break
-                elif _map[dR[0]][dR[1]+1] == '.':
-                    dR[1] += 1
-                elif _map[dR[0]][dR[1]+1] == '#':
-                    break
-                elif _map[dR[0]][dR[1]+1] == 'O':
-                    red_into_the_hole = True
-                    dR = (-1, -1)
-                    break
-            for i in range(dB[1], M-1, 1):
-                if dR[0] == dB[0] and dR[1] == (dB[1]+1):
-                    break
-                elif _map[dB[0]][dB[1]+1] == '.':
-                    dB[1] += 1
-                elif _map[dB[0]][dB[1]+1] == '#':
-                    break
-                elif _map[dB[0]][dB[1]+1] == 'O':
-                    blue_into_the_hole = True
-                    break
-            if blue_into_the_hole is False and red_into_the_hole is True:
-                return 1
-            if red_into_the_hole is False and blue_into_the_hole is False:
-                q.append((moves+1, (dR[0], dR[1]), (dB[0], dB[1])))
     return 0
 
 def run():
