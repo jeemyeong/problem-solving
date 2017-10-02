596afa781393b63da24f155ff4e8d09bc9663574
jeemyeong <jeemyeong@gmail.com>
2017-10-03 01:38:17 +0900

    3017

---
+++ boj/3017/3017.py
@@ -0,0 +1,24 @@
+# Not solved
+# def solve(a, b):
+#     count_b = dict()
+#     for i in range(10):
+#         count_b[i] = 0
+#     n = 0
+#     while b > 0:
+#         n+=1
+#         count_b[b%10] += 1
+#         b //= 10
+#     bigger = 0
+#     smaller = 0
+#     for i in range(n-1, -1, -1):
+#         a//(10**i)
+#     return -1
+
+# def run():
+#     import sys
+#     read = sys.stdin.readline
+#     a = int(read().replace("\n", ""))
+#     b = int(read().replace("\n", ""))
+#     print(solve(a, b))
+
+# run()
