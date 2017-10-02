5dbf5b376604866ca5afcdbe4665d3b0363841be
jeemyeong <jeemyeong@gmail.com>
2017-10-02 17:06:11 +0900

    13460

---
+++ boj/13460/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("./13460-1.input"));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Hello world");
+    }
+}
