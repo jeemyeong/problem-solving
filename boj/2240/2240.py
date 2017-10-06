b875508bef45d592832a8f5cf052197826bbdd6b
jeemyeong <jeemyeong@gmail.com>
2017-10-06 22:34:37 +0900

    add snippet

--- boj/2240/2240.py
+++ boj/2240/2240.py
@@ -1,3 +1,19 @@
+import sys
+# import heapq, collections
+
+sys.setrecursionlimit(10**7)
+inf = 10**20
+eps = 1.0 / 10**15
+mod = 10**9+7
+
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline())
+def F(): return float(sys.stdin.readline())
+def S(): return input()
+
 def get_item_without_null_exception(i, k, p, dp):
     if i < 0 or k < 0 or p < 0:
         return 0
@@ -18,12 +34,8 @@ def solve(T, W, order):
     return max(max(dp[T-1], key=lambda x: max(x)))
 
 def run():
-    import sys
-    read = sys.stdin.readline
-    T, W = map(int, read().split())
-    order = []
-    for _ in range(T):
-        order.append(int(read()))
+    T, W = LI()
+    order = [I() for _ in range(T)]
     print(solve(T, W, order))
 
 run()
