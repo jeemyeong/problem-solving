fa2a6d32fba652c052473cd477aed00639165282
jeemyeong <jeemyeong@gmail.com>
2017-10-08 16:35:03 +0900

    max_index

---
+++ geeks/maximum_index.py
@@ -0,0 +1,32 @@
+import sys
+# import heapq, collections
+
+sys.setrecursionlimit(10**7)
+inf = 10**20
+eps = 1.0 / 10**15
+mod = 10**9+7
+
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline())
+def F(): return float(sys.stdin.readline())
+def S(): return input()
+
+def solve(n, seq):
+    ans = 0
+    for i in range(n-1, 0, -1):
+        for j in range(0, n-i):
+            if seq[j] <= seq[j+i]:
+                return i
+    return ans
+
+def main():
+    t = I()
+    for _ in range(t):
+        n = I()
+        seq = LI()
+        print(solve(n, seq))
+
+main()
