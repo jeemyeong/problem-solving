294b03d78c6b0b82bcc795228cfed5e2b0a78fd1
jeemyeong <jeemyeong@gmail.com>
2017-10-17 18:51:02 +0900

    woowahan mock

---
+++ woowahan/mock.js
@@ -0,0 +1,21 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 6.4.0)
+    set = new Set();
+    for (var i = 0; i < A.length; i++) {
+        set.add(A[i]);
+    }
+    for (var i = 1; i < 100002; i++) {
+        if (set.has(i)) {
+            continue;
+        } else {
+            console.log(i);
+            return;
+        }
+    }
+
+}
+
+solution([1, 3, 6, 4, 1, 2])
