0db13470d32547e8c45672b5571fcca02e713e2c
jeemyeong <jeemyeong@gmail.com>
2017-11-27 04:21:59 +0900

    add 1.py with java ver in maester

--- maester/1.py
+++ maester/1.py
@@ -5,3 +5,15 @@ def solution(nums):
 print(solution([3,1,2,3]))
 print(solution([3,3,3,2,2,4]))
 print(solution([3,3,3,2,2,2]))
+
+# Java ver
+# import java.util.Arrays;
+# import java.util.List;
+# import java.util.stream.Collectors;
+
+# class Solution {
+#     public int solution(int[] nums) {
+#         List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+#         return (int) Math.min(list.parallelStream().distinct().count(), nums.length/2);
+#     }
+# }
