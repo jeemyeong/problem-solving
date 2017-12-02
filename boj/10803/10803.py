87a18d18e7494c647b286cedd5ba83cf7cf7d4ea
jeemyeong <jeemyeong@gmail.com>
2017-12-02 16:51:17 +0900

    10803

--- boj/10803/10803.py
+++ boj/10803/10803.py
@@ -25,14 +25,16 @@ def solve(n, m):
         dp[i][0] = 0
     for y in range(1, m+1):
         for x in range(1, n+1):
+            if (x >= 4 * y):
+                dp[y][x] = min(dp[y][x], dp[y][y-x] + 1)
+                continue
             if y == x:
                 dp[y][x] = 1
                 continue
-            size = min(y,x)+1
-            for dy in range(size):
-                dx = dy
-                dp[y][x] = min(dp[y][x], dp[dy][dx] + dp[y-dy][dx] + dp[y][x-dx])
-                dp[y][x] = min(dp[y][x], dp[dy][dx] + dp[dy][x-dx] + dp[y-dy][x])
+            for dy in range(y+1):
+                dp[y][x] = min(dp[y][x], dp[dy][x] + dp[y-dy][x])
+            for dx in range(x+1):
+                dp[y][x] = min(dp[y][x], dp[y][dx] + dp[y][x-dx])
     return dp[m][n]
 
 # print(solve(6, 5))
