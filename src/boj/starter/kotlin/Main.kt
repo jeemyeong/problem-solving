fb763366599bb9dffaa0f00c68f617bba410cf31
jeemyeong <jeemyeong@gmail.com>
2018-02-17 17:42:05 +0900

    Update starter

--- src/boj/starter/kotlin/Main.kt
+++ src/boj/starter/kotlin/Main.kt
@@ -8,24 +8,21 @@ import java.util.stream.*
 
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
     var st: StringTokenizer
 
-    val T = Integer.parseInt(br.readLine())
+    val T = br.readLine().toInt()
     for (i in 0 until T) {
         st = StringTokenizer(br.readLine())
-        val N = Integer.parseInt(st.nextToken())
-        val M = Integer.parseInt(st.nextToken())
+        val N = st.nextToken().toInt()
+        val M = st.nextToken().toInt()
         val map = Array(N) { IntArray(M) }
         for (j in 0 until N) {
             map[j] = Stream.of(*br.readLine().split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()).mapToInt(ToIntFunction<String> { Integer.parseInt(it) }).toArray()
         }
         val ans = solve(N, M, map)
-        bw.write(ans.toString() + "\n")
+        println(ans.toString())
     }
     br.close()
-    bw.flush()
-    bw.close()
 }
 
 fun solve(N: Int, M: Int, map: Array<IntArray>): Int {
