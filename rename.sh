716d4879fdf179fcc1893351ca665331ef9ed482
jeemyeong <jeemyeong@gmail.com>
2018-02-17 15:48:21 +0900

    renamer

--- rename.sh
+++ rename.sh
@@ -15,18 +15,27 @@
 #   done
 # done
 
-for d in $(find ./boj/* -maxdepth 1 -type d)
+# for d in $(find ./boj/* -maxdepth 1 -type d)
+# do
+#   #Do something, the directory is accessible with $d:
+#   echo $d
+#   for file in $d/*.input
+#   do
+#     printf $file
+#     printf "\n"
+#     new=$d/${file//*-/}
+#     new=${new//input/test}
+#     printf $new
+#     printf "\n"
+#     mv $file $new
+#   done
+# done
+
+for d in $(find ./src/boj/* -maxdepth 1 -type d)
 do
   #Do something, the directory is accessible with $d:
   echo $d
-  for file in $d/*.input
-  do
-    printf $file
-    printf "\n"
-    new=$d/${file//*-/}
-    new=${new//input/test}
-    printf $new
-    printf "\n"
-    mv $file $new
-  done
+  new=${d/boj\//boj/boj}
+  echo $new
+  mv $d $new
 done
