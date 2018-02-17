e496b5fa5b15da5fb6f788113a0922237ffb2358
jeemyeong <jeemyeong@gmail.com>
2018-02-17 15:49:07 +0900

    renamer

--- rename.sh
+++ rename.sh
@@ -1,39 +1,36 @@
 #!/bin/sh
 
-# for d in $(find ./boj/* -maxdepth 1 -type d)
-# do
-#   #Do something, the directory is accessible with $d:
-#   echo $d
-#   for file in $d/*.{js,py,clj}
-#   do
-#     printf $file
-#     printf "\n"
-#     new=$d/${file//*./Main.}
-#     printf $new
-#     printf "\n"
-#     mv $file $new
-#   done
-# done
+for d in $(find ./boj/* -maxdepth 1 -type d)
+do
+  echo $d
+  for file in $d/*.{js,py,clj}
+  do
+    printf $file
+    printf "\n"
+    new=$d/${file//*./Main.}
+    printf $new
+    printf "\n"
+    mv $file $new
+  done
+done
 
-# for d in $(find ./boj/* -maxdepth 1 -type d)
-# do
-#   #Do something, the directory is accessible with $d:
-#   echo $d
-#   for file in $d/*.input
-#   do
-#     printf $file
-#     printf "\n"
-#     new=$d/${file//*-/}
-#     new=${new//input/test}
-#     printf $new
-#     printf "\n"
-#     mv $file $new
-#   done
-# done
+for d in $(find ./boj/* -maxdepth 1 -type d)
+do
+  echo $d
+  for file in $d/*.input
+  do
+    printf $file
+    printf "\n"
+    new=$d/${file//*-/}
+    new=${new//input/test}
+    printf $new
+    printf "\n"
+    mv $file $new
+  done
+done
 
 for d in $(find ./src/boj/* -maxdepth 1 -type d)
 do
-  #Do something, the directory is accessible with $d:
   echo $d
   new=${d/boj\//boj/boj}
   echo $new
