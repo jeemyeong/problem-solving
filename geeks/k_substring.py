4f0603eeb59c6504db04928fa32cb22fa99bad88
jeemyeong <jeemyeong@gmail.com>
2018-04-17 02:14:51 +0900

    Update k_substring

--- geeks/k_substring.py
+++ geeks/k_substring.py
@@ -2,35 +2,21 @@ import sys
 read = sys.stdin.readline
 
 def longestSubstringWithKUniqueCharacters(string, k):
-    ret = ""
-    ret_size = 0
-    n = len(string)
-    _hash = {}
-    unique = 0
-    lastIdx = 0
-    for i in range(n):
-        new = string[i]
-        if (new not in _hash) or (new in _hash and _hash[new]==0):
-            _hash[new] = 1
-            unique += 1
-        else:
-            _hash[new] += 1
-        while unique > k:
-            last = string[lastIdx]
-            if _hash[last] > 1:
-                lastIdx += 1
-                _hash[last] -= 1
-            else:
-                lastIdx += 1
-                _hash[last] = 0
-                unique -= 1
-        if unique == k and i+1-lastIdx > ret_size:
-            ret = string[lastIdx:i+1]
-            ret_size = i+1-lastIdx
-    if ret != "":
-        return ret_size
-    else:
-        return -1
+    char_set= {}
+    left = 0; right = 0
+    ret = ""; u = 0; length = len(string)
+    while right < length:
+        if (not string[right] in char_set) or (char_set[string[right]] == 0):
+            u+=1; char_set[string[right]] = 0
+        char_set[string[right]] += 1
+        right += 1
+        while u > k:
+            char_set[string[left]] -= 1
+            if char_set[string[left]] == 0: u-=1
+            left += 1
+        if right-left > len(ret):
+            ret = string[left:right]
+    return ret
 
 def run():
     n = int(read().replace("\n", ""))
